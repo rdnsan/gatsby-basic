3104885f6147c2f76b8af9504f406ae4d91f7747
Ridwan Ikhsan <ridwan@oneyel.com>
2021-09-15 03:35:33 +0700

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.jsx
+++ src/pages/blog/{mdx.slug}.jsx
@@ -1,25 +1,35 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../../components/layout';
 
 const BlogPost = ({ data }) => {
+  const images = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={images} alt={data.mdx.frontmatter.hero_image_alt} />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: { eq: $id }) {
+  query ($slug: String) {
+    mdx(slug: { eq: $slug }) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `;
