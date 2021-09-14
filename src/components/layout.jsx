27a50ef7c8b253fd2b8b583e196b968cb527acfb
Ridwan Ikhsan <ridwan@oneyel.com>
2021-09-15 03:57:36 +0700

    Change styling to styled component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
-} from './layout.module.css';
+
+// Style
+const container = {
+  margin: 'auto',
+  maxWidth: '500px',
+  fontFamily: 'sans-serif',
+};
+
+const heading = {
+  color: 'rebeccapurple',
+};
+
+const navLinks = {
+  display: 'flex',
+  listStyle: 'none',
+  paddingLeft: '0',
+};
+
+const navLinkItem = {
+  paddingRight: '2rem',
+};
+
+const navLinkText = {
+  color: 'black',
+};
+
+const siteTitle = {
+  fontSize: '3rem',
+  color: 'gray',
+  fontWeight: '700',
+  margin: '3rem 0',
+};
 
 const Layout = ({ pageTitle, children }) => {
   // GraphQL syntax
@@ -22,32 +46,32 @@ const Layout = ({ pageTitle, children }) => {
   `);
 
   return (
-    <div className={container}>
+    <div style={container}>
       <title>
         {pageTitle} | {data.site.siteMetadata.title}
       </title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <header style={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to='/' className={navLinkText}>
+        <ul style={navLinks}>
+          <li style={navLinkItem}>
+            <Link to='/' style={navLinkText}>
               Home
             </Link>
           </li>
-          <li className={navLinkItem}>
-            <Link to='/about' className={navLinkText}>
+          <li style={navLinkItem}>
+            <Link to='/about' style={navLinkText}>
               About
             </Link>
           </li>
-          <li className={navLinkItem}>
-            <Link to='/blog' className={navLinkText}>
+          <li style={navLinkItem}>
+            <Link to='/blog' style={navLinkText}>
               Blog
             </Link>
           </li>
         </ul>
       </nav>
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
+        <h1 style={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
